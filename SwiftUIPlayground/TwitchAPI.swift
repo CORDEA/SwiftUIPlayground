1a6c07fdc90b4fdcc2b5f2cf07fe90b24a25044e
Yoshihiro Tanaka <contact@cordea.jp>
2020-05-06 16:49:23 +0900

    set headers

--- SwiftUIPlayground/TwitchAPI.swift
+++ SwiftUIPlayground/TwitchAPI.swift
@@ -12,14 +12,24 @@ import Foundation
 class TwitchAPI {
     static let shared = TwitchAPI()
     private static let baseURL: URL = URL(string: "https://api.twitch.tv/kraken/")!
+    private static let twitchClientID: String = Bundle.main.infoDictionary!["Twitch Client ID"] as! String
 
     private init() {
     }
 
     func getTopGames(limit: Int) -> DataRequest {
+        let headers: HTTPHeaders = [
+            "Accept": "application/vnd.twitchtv.v5+json",
+            "Client-ID": TwitchAPI.twitchClientID
+        ]
         let params: Parameters = [
             "limit": limit
         ]
-        return AF.request(TwitchAPI.baseURL.appendingPathComponent("/games/top"), method: .get, parameters: params)
+        return AF.request(
+                TwitchAPI.baseURL.appendingPathComponent("/games/top"),
+                method: .get,
+                parameters: params,
+                headers: headers
+        )
     }
 }
