0a43386957e836c9be44ea9b93e0d9932137021e
Yoshihiro Tanaka <contact@cordea.jp>
2020-05-06 15:37:04 +0900

    define list item

---
+++ SwiftUIPlayground/TopGameRow.swift
@@ -0,0 +1,35 @@
+//
+//  TopGameRow.swift
+//  SwiftUIPlayground
+//
+//  Created by Yoshihiro Tanaka on 5/6/20.
+//  Copyright © 2020 Yoshihiro Tanaka. All rights reserved.
+//
+
+import SwiftUI
+import URLImage
+
+struct TopGameRow: View {
+    var model: TopGameRowModel
+
+    var body: some View {
+        HStack {
+            URLImage(model.imageURL) { proxy in
+                proxy.image
+                        .resizable()
+                        .frame(width: 100.0, height: 56.0)
+            }.padding(16.0)
+            VStack(alignment: .leading, spacing: 2.0) {
+                Text(model.name).font(.body)
+                Text(model.formattedNumberOfViewers()).font(.caption)
+            }
+            Spacer()
+        }.frame(minHeight: 72.0)
+    }
+}
+
+struct TopGameRow_Previews: PreviewProvider {
+    static var previews: some View {
+        TopGameRow(model: TopGameRowModel(numberOfViewers: 1209, name: "Game", imageURL: URL(string: "https://example.com")!))
+    }
+}
