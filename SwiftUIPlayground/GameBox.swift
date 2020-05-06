4711661b4919d527fc3515c4a99c7c69eb630358
Yoshihiro Tanaka <contact@cordea.jp>
2020-05-06 14:36:37 +0900

    define game box

---
+++ SwiftUIPlayground/GameBox.swift
@@ -0,0 +1,16 @@
+//
+//  GameBox.swift
+//  SwiftUIPlayground
+//
+//  Created by Yoshihiro Tanaka on 5/6/20.
+//  Copyright © 2020 Yoshihiro Tanaka. All rights reserved.
+//
+
+import Foundation
+
+struct GameBox: Decodable {
+    var large: String
+    var medium: String
+    var small: String
+    var template: String
+}
