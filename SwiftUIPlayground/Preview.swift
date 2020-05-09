6ba80b78b6a55c58b452d6e9ab9b82e42b271475
Yoshihiro Tanaka <contact@cordea.jp>
2020-05-09 20:08:02 +0900

    define preview

---
+++ SwiftUIPlayground/Preview.swift
@@ -0,0 +1,16 @@
+//
+//  Preview.swift
+//  SwiftUIPlayground
+//
+//  Created by Yoshihiro Tanaka on 5/9/20.
+//  Copyright © 2020 Yoshihiro Tanaka. All rights reserved.
+//
+
+import Foundation
+
+struct Preview: Decodable {
+    var large: String
+    var medium: String
+    var small: String
+    var template: String
+}
