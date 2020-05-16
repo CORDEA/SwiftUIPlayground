9fe501a7705f3a0fe3fb89d3aed49cbfe69b8b59
Yoshihiro Tanaka <contact@cordea.jp>
2020-05-16 17:46:23 +0900

    resize image

--- SwiftUIPlayground/TopGameRow.swift
+++ SwiftUIPlayground/TopGameRow.swift
@@ -17,7 +17,8 @@ struct TopGameRow: View {
             URLImage(model.imageURL) { proxy in
                 proxy.image
                         .resizable()
-                        .frame(width: 100.0, height: 56.0)
+                        .scaledToFit()
+                        .frame(width: 56.0, height: 56.0)
             }.padding(16.0)
             VStack(alignment: .leading, spacing: 2.0) {
                 Text(model.name).font(.body)
